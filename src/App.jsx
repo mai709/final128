df839713336724f0f7bdd502a7f21f072e5d1eef
mai709 <hassanmai423@gmail.com>
2024-10-21 21:21:03 +0300

    update navbar and footer notfound

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { useState } from 'react'
 import './App.css'
 
 import Home from './components/Home/Home'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+import NotFound from './components/NotFound/NotFound'
 
 function App() {
   const [count, setCount] = useState(0)
+  let routers = createBrowserRouter([
 
-  return (
-    <>
-     <Home />
-    </>
-  )
+    {path:"", element:<Layout/>,children:[
+      {index :true,element:<Home />},
+      {path:"*" ,element:<NotFound/>}
+    ] }
+  ])
+  return <RouterProvider router={routers}></RouterProvider>
+  
 }
 
 export default App
